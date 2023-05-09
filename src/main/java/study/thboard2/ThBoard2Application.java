cbaee68d2d6668490c4ea077bc43f5de4c54d08e
devlsy86 <infightersy@naver.com>
2023-05-09 22:48:14 +0900

    첫번 째 푸시

---
+++ src/main/java/study/thboard2/ThBoard2Application.java
@@ -0,0 +1,13 @@
+package study.thboard2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThBoard2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThBoard2Application.class, args);
+    }
+
+}
