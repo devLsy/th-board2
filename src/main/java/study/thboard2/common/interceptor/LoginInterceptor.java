81dd4af3d425e0df073efc3fa40abe4ef7fef689
devlsy86 <infightersy@naver.com>
2023-05-13 15:45:53 +0900

    로그인 인터셉터 적용

---
+++ src/main/java/study/thboard2/common/interceptor/LoginInterceptor.java
@@ -0,0 +1,29 @@
+package study.thboard2.common.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@Slf4j
+//로그인 인터셉터
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        String requestURI = request.getRequestURI();
+        log.info("interceptor_requestURI = [{}]", requestURI);
+
+        HttpSession session = request.getSession();
+        if ("none".equals(session.getAttribute("id")) || session.getAttribute("id") == null) {
+            log.info("로그인을 안했네? 로그인으로 가버려~~~~~~~~~");
+            response.sendRedirect("/login");
+            return false;
+        }
+        log.info("옳지! 로그인 상태구나. 너 하고 싶은데로 가라.");
+        return true;
+    }
+}
---
+++ src/main/java/study/thboard2/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package study.thboard2.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import study.thboard2.common.interceptor.LoginInterceptor;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        LoginInterceptor loginInterceptor = new LoginInterceptor();
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/login", "logout", "/css/**", "/asset/**", "/js/**");
+    }   
+}
