ecde5f859e870d63505bc7ac68d0d71ec7ab82b2
devlsy86 <infightersy@naver.com>
2023-05-14 16:48:56 +0900

    content script 위치 변경, 댓글 기능 화면단 개발 중

--- src/main/java/study/thboard2/controller/ReplyController.java
+++ src/main/java/study/thboard2/controller/ReplyController.java
@@ -2,10 +2,10 @@ package study.thboard2.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import study.thboard2.domain.vo.ReplyVo;
@@ -14,6 +14,7 @@ import study.thboard2.service.ReplyService;
 @RestController
 @Slf4j
 @RequiredArgsConstructor
+@RequestMapping("reply/*")
 public class ReplyController {
 
     private final ReplyService replyService;
@@ -25,15 +26,15 @@ public class ReplyController {
      * @return
      * @throws Exception
      */
-    @PostMapping("/reply/{boardNo}")
-    public ResponseEntity saveReply(@PathVariable Integer boardNo, ReplyVo replyVo, RedirectAttributes rttr) throws Exception{
+    @PostMapping(value = "reg/{boardNo}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public Integer saveReply(@PathVariable Integer boardNo, ReplyVo replyVo, RedirectAttributes rttr) throws Exception{
         try {
             replyService.regReply(replyVo);
         } catch (Exception e) {
             log.info("Exception => [{}] ", e.getMessage());
         }
         rttr.addFlashAttribute("replyNo", replyVo.getReplyNo());
-        return new ResponseEntity<>(HttpStatus.OK, rttr);
+        return replyVo.getReplyNo();
     }
 
 
