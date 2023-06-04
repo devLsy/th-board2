13058ac3d6e74850619e9cf8f8481366d3a7c3c3
devlsy86 <infightersy@naver.com>
2023-06-04 13:58:12 +0900

    테스트 코드 추가

---
+++ src/test/java/study/thboard2/config/SecurityConfigTest.java
@@ -0,0 +1,35 @@
+package study.thboard2.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Slf4j
+class SecurityConfigTest {
+
+    @Autowired
+    private SecurityConfig securityConfig;
+
+    @Test
+    @DisplayName("SecurityConfigTest")
+    @Commit
+    public void SecurityConfigTest() throws Exception {
+        //givin
+        String rawPw = "1234";
+        String encodePw = securityConfig.bCryptPasswordEncoder().encode(rawPw);
+        String inputPw = "123";
+
+        //when
+        boolean check = securityConfig.bCryptPasswordEncoder().matches(inputPw, encodePw);
+        log.info("check = " + check);
+        //then
+    }
+
+}
