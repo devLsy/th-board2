65a016a4e9255476f880ce280872cc484a2db703
devlsy86 <infightersy@naver.com>
2023-06-04 12:56:25 +0900

    스프링 시큐리티 암호화 적용 준비

--- src/main/java/study/thboard2/ThBoard2Application.java
+++ src/main/java/study/thboard2/ThBoard2Application.java
@@ -2,8 +2,9 @@ package study.thboard2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class ThBoard2Application {
 
     public static void main(String[] args) {
